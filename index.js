36f04e7d72cbf7c4c29e78f82ebc8c073ceba135
Melvin renversez <120313822+melvinRenversez@users.noreply.github.com>
2025-02-24 20:50:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+          res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', (socket) => {
+
+          socket.on('message', (msg) => {
+                    console.log('message:', msg);
+          })
+          
+})
+
+
+
+server.listen(3000, () => {
+          console.log('Server is running on port 3000');
+});
